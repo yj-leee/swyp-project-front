f8cba59cd6df72956e6af19570c0c71d6e0faab8
yj-leee <yj.leee1010@gmail.com>
2025-01-30 17:07:19 +0900

    test: branch ci test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('This is a test commit for CI.');
+
   return (
     <div className="App">
       <header className="App-header">
